57c183124a1e5c37c4df36608bd2878758894c66
Nima Movasseghi <nmovasseghi94@gmail.com>
2020-03-02 18:25:00 -0600

    added components main and header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+
+function Header() {
+    
+    return(
+        <>
+            <h2>Hello From Header</h2>
+        </>
+    );
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+import Header from './Header';
+
+
+function Main() {
+    return(
+        <>  
+            <Header/>
+            <h1>Hello World</h1>
+        </>
+    );
+}
+
+export default Main;
