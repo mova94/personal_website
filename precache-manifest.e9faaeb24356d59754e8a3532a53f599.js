58eeddc436e5da3fd48bfea588f1f70d92f0ca8d
Nima Movasseghi <mova@Nimas-MacBook-Pro.local>
2020-03-11 12:11:58 -0500

    Updates

---
+++ precache-manifest.e9faaeb24356d59754e8a3532a53f599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c258d6769342e5d8efbb3616ad9bbca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d486c1da74e831c65a9b",
+    "url": "/static/css/2.b8c81c1a.chunk.css"
+  },
+  {
+    "revision": "ebcff2c9c1ddf36444c9",
+    "url": "/static/css/main.b50b32e3.chunk.css"
+  },
+  {
+    "revision": "d486c1da74e831c65a9b",
+    "url": "/static/js/2.22f7dc73.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.22f7dc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebcff2c9c1ddf36444c9",
+    "url": "/static/js/main.886e9f1c.chunk.js"
+  },
+  {
+    "revision": "39d6f10df612a0f522ac",
+    "url": "/static/js/runtime-main.e9aa8d15.js"
+  },
+  {
+    "revision": "8416a2b76db9048b86d70ca45d3ab286",
+    "url": "/static/media/me.8416a2b7.jpg"
+  }
+]);
