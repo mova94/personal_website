42fe897dc7930df31848004fbee2c5d98fd31007
Nima Movasseghi <mova@Nimas-MacBook-Pro.local>
2020-03-11 12:13:39 -0500

    Updates

---
+++ precache-manifest.709bc75302a46e7e51d199e849966162.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19445b0e1ebf4b14ed82ae52e27ff989",
+    "url": "/personal_website/index.html"
+  },
+  {
+    "revision": "d486c1da74e831c65a9b",
+    "url": "/personal_website/static/css/2.b8c81c1a.chunk.css"
+  },
+  {
+    "revision": "ebcff2c9c1ddf36444c9",
+    "url": "/personal_website/static/css/main.b50b32e3.chunk.css"
+  },
+  {
+    "revision": "d486c1da74e831c65a9b",
+    "url": "/personal_website/static/js/2.22f7dc73.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/personal_website/static/js/2.22f7dc73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebcff2c9c1ddf36444c9",
+    "url": "/personal_website/static/js/main.886e9f1c.chunk.js"
+  },
+  {
+    "revision": "ce53dcebb4e1ee39fb8e",
+    "url": "/personal_website/static/js/runtime-main.77860c49.js"
+  },
+  {
+    "revision": "8416a2b76db9048b86d70ca45d3ab286",
+    "url": "/personal_website/static/media/me.8416a2b7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e9faaeb24356d59754e8a3532a53f599.js"
+  "/personal_website/precache-manifest.709bc75302a46e7e51d199e849966162.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal_website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
